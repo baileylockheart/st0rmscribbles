3613f42acdf487625e8af95e6034df2441b67c6f
Bailey Lockheart <baileylockheart@gmail.com>
2025-01-20 13:32:21 -0800

    Updated navigation

---
+++ site/nav.js
@@ -0,0 +1,8 @@
+document.getElementById("nav").innerHTML = `
+<ul>
+    <li><a href='/characters.html'>Characters</a></li>
+    <li><a href='/stuff.html'>Stuff</a></li>
+    <li><a href='/links.html'>Links</a></li>
+</ul>
+`;
+
