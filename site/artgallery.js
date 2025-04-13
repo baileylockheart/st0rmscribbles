9d7aa6ef60507b61567208ae686e80a5ca29e2a2
Bailey <baileylockheart@gmail.com>
2025-04-13 15:38:33 -0700

    I drew something :O

---
+++ site/artgallery.js
@@ -0,0 +1,59 @@
+document.getElementById("artgallery").innerHTML = `
+<style>
+    #center{
+        text-align: center;
+        margin: 0 auto;
+    }
+    .gallery{
+        height: 8rem;
+        border: 2px solid var(--text);
+        border-radius: 10px;
+    }
+    .gallery:hover{
+        -webkit-transform:scale(2);
+        transform:scale(2);
+        transition: all .3s ease-out;
+        z-index: 9999999;
+    }
+    .vlarge:hover{
+        transform:scale(3);}
+</style>
+ <div id="center">
+    <hr>
+    <h3>2025</h3>
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2025/strawberry.png">
+    <hr>
+    <h3>2024</h3>
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2024/Elaila%20Half%20Body%208-9-2024.png">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2024/Sky%20Half%20Body%208-8-2024.png">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2024/SSS%20Angst%208-4-2024.png">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2024/Sunni%20Half%20Body%208-4-2024.png">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2024/Flint%20Bust%207-17-2024.png">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/ref%20sheets/Lym%20Ref%207-9-2024.png">
+    <hr>
+    <h3>2023</h3>
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2023/CAT%2012-16-2023.png">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2023/Koya%2010-16-2023.PNG">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2023/Sad%20Turtle%2010-6-2023.png">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2023/Arms%2010-5-2023.png">
+    <hr>
+    <h3>2022</h3>
+    <img class="gallery vlarge" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2022/Scara%2012-16-2022.png">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2022/Kae%2012-15-2022.png">
+    <img class="gallery vlarge" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2022/webtoon%205-2-2022.png">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2022/Guy%204-30-2022.png">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2022/Kuroo%204-27-2022.png">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2022/Takumi%20Aldini%204-24-2022.png">
+    <hr>
+    <h3>2021</h3>
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2021/Deku%2012-20-2021.png">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2021/webtoon%20characters%2012-17-2021.png">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2021/Worried%20Webtoon%2012-17-2021.png">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2021/Traditional%20Dabi%209-28-2021.jpg">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2021/Human!Pepsi%202-25-2021.png">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2021/Toi%20Kuji%202-22-2021.png">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2021/TikTok%20Challenge%201-28-2021.png">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2021/Angry%201-24-2021.png">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2021/Anime%20Girl%201-17-2021.png">
+</div>
+`;
