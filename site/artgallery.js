484cf74356573b9e325ac0c734a155bf128cda10
Bailey <baileylockheart@gmail.com>
2025-04-25 13:11:08 -0700

    Added titles to characters

--- site/artgallery.js
+++ site/artgallery.js
@@ -21,7 +21,7 @@ document.getElementById("artgallery").innerHTML = `
  <div id="center">
     <hr>
     <h3>2025</h3>
-    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2025/Katsu%204-19-2025.png" title="Fanart of Katsu by Salem Wasteland">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2025/Katsu%204-19-2025.png" title="Katsu by Salem Wasteland">
     <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2025/Niko%202.0%204-15-2025.png">
     <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2025/Niko%204-13-2025.png">
     <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2025/strawberry%204-13-2025.png">
@@ -36,25 +36,25 @@ document.getElementById("artgallery").innerHTML = `
     <hr>
     <h3>2023</h3>
     <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2023/CAT%2012-16-2023.png">
-    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2023/Koya%2010-16-2023.PNG">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2023/Koya%2010-16-2023.PNG" title="BT21 Koya">
     <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2023/Sad%20Turtle%2010-6-2023.png">
     <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2023/Arms%2010-5-2023.png">
     <hr>
     <h3>2022</h3>
-    <img class="gallery vlarge" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2022/Scara%2012-16-2022.png">
-    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2022/Kae%2012-15-2022.png">
+    <img class="gallery vlarge" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2022/Scara%2012-16-2022.png" title="Scara from Genshin Impact">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2022/Kae%2012-15-2022.png" title="Kaeya from Genshin Impact">
     <img class="gallery vlarge" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2022/webtoon%205-2-2022.png">
     <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2022/Guy%204-30-2022.png">
-    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2022/Kuroo%204-27-2022.png">
-    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2022/Takumi%20Aldini%204-24-2022.png">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2022/Kuroo%204-27-2022.png" title="Kuroo from Haikyuu">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2022/Takumi%20Aldini%204-24-2022.png" title="Takumi from Food Wars">
     <hr>
     <h3>2021</h3>
-    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2021/Deku%2012-20-2021.png">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2021/Deku%2012-20-2021.png" title="Deku from BNHA">
     <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2021/webtoon%20characters%2012-17-2021.png">
     <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2021/Worried%20Webtoon%2012-17-2021.png">
-    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2021/Traditional%20Dabi%209-28-2021.jpg">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2021/Traditional%20Dabi%209-28-2021.jpg" title="Dabi from BNHA">
     <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2021/Human!Pepsi%202-25-2021.png">
-    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2021/Toi%20Kuji%202-22-2021.png">
+    <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2021/Toi%20Kuji%202-22-2021.png" title="Toi from Sarazanmai">
     <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2021/TikTok%20Challenge%201-28-2021.png">
     <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2021/Angry%201-24-2021.png">
     <img class="gallery" src="https://raw.githubusercontent.com/baileylockheart/st0rmscribbles/refs/heads/main/general%20art/2021/Anime%20Girl%201-17-2021.png">
